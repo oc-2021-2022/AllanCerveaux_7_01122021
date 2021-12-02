5671599519672db528020d3201fd10ae389aad9b
Callan <cerveauxallanjean@gmail.com>
2021-12-02 17:08:18 +0100

    Change device

--- src/scripts/CardGenerator.js
+++ src/scripts/CardGenerator.js
@@ -1,9 +1,8 @@
-import Manipulator from './lib/Manipulator.js';
 
-export class CardGenerator {
-  static render({ name, image = '', ingredients, time, description }) {
-    return `
-      <div class="column is-5">
+
+export const CardGenerator = ({ name, image = '', ingredients, time, description }) => {
+    return /* html */`
+      <article class="column is-4">
         <div class="card">
           <div class="card-image has-background-grey-light">
             <figure class="image is-4by3">
@@ -13,7 +12,7 @@ export class CardGenerator {
           <div class="card-content has-background-white-ter">
             <div class="media is-flex is-align-items-center is-justify-content-space-between">
               <div class="media-left is-flex-grow-1">
-                <h3 class="title is-4">${name}</h3>
+                <h3 class="title is-5">${name}</h3>
               </div>
               <div class="media-content is-flex-grow-0">
                 <p class="title is-5"> 
@@ -25,13 +24,12 @@ export class CardGenerator {
 
             <div class="content columns">
               <ul class="column">
-                ${ingredients.map(({ingredient, quantity, unit}) => `<li>${ingredient} ${quantity} ${unit}</li>`)}
-              </div>
+                ${ingredients.map(({ingredient, quantity, unit}) => `<li>${ingredient} ${quantity ?? ''} ${unit ?? ''}</li>`).join(' ')}
+              </ul>
               <div class="column">${description}</div>
             </div>
           </div>
         </div>
-      </div>
+      </article>
     `
   }
-}
