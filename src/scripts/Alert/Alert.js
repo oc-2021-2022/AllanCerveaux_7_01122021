b219bc18ae966c7033e107250233ed48bd7aa25e
Callan <cerveauxallanjean@gmail.com>
2021-12-08 10:31:41 +0100

    Create alert

---
+++ src/scripts/Alert/Alert.js
@@ -0,0 +1,23 @@
+export class Alert {
+  constructor(type, message) {
+    this.type = type
+    this.message = message
+  }
+
+  template() {
+    return /* html */`
+      <div class="alert alert-${this.type}" id="search-alert" role="alert">
+        <h2>${this.message}</h2>
+      </div>
+    `
+  }
+
+  display(parent) {
+    console.log(parent, document.getElementById('search-alert'))
+    if(document.getElementById('search-alert') === null) parent.append(this.template())
+  }
+
+  static remove() {
+    document.getElementById('search-alert').remove()
+  }
+}
