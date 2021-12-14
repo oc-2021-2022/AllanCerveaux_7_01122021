1b94e611ebfc1816d8dddaa30e81052c7791b1a2
Callan <cerveauxallanjean@gmail.com>
2021-12-14 16:53:17 +0100

    Feature: add algo for sort by ustensil and appliance

--- src/scripts/Search/search-algo/functional.js
+++ src/scripts/Search/search-algo/functional.js
@@ -1,8 +1,6 @@
 import { recipes } from '../../../resources/data/recipes.json'
 
-export const search_by_name = (term) => recipes.filter(({ name }) => {
-  return name.toLowerCase().includes(term.toLowerCase())
-})
+export const search_by_name = (term) => recipes.filter(({ name }) => name.toLowerCase().includes(term.toLowerCase()))
 
 
 export const search_by_ingredient = (term) => recipes.filter(({ ingredients }) => {
@@ -11,3 +9,7 @@ export const search_by_ingredient = (term) => recipes.filter(({ ingredients }) =
     return ingredients.includes(...result)
   }
 })
+
+export const search_by_ustensil = (term) => recipes.filter(({ ustensils }) => ustensils.includes(term))
+
+export const search_by_appliance = (term) => recipes.filter(({ appliance }) => appliance.toLowerCase().includes(term))
--- src/scripts/Search/search-algo/native.js
+++ src/scripts/Search/search-algo/native.js
@@ -5,10 +5,53 @@ export const search_by_name = (term) => {
   for (let i = 0; i < recipes.length; i++) {
     const recipe = recipes[i];
     if (recipe.hasOwnProperty('name')) {
-      if (recipe.name.charAt(0).toLocaleLowerCase() === term.charAt(0)) {
-        if (recipe.name.toLocaleLowerCase().includes(term)) {
-          result.push(recipe)
+      if (recipe.name.toLocaleLowerCase().includes(term)) {
+        result.push(recipe)
+      }
+    }
+  }
+  return result
+}
+
+export const search_by_ingredient = (term) => {
+  const result = []
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    if (recipe.hasOwnProperty('ingredients')) {
+      const ingredients = recipes[i].ingredients
+      for (const key in ingredients) {
+        const ingredient = ingredients[key];
+        if (ingredient.hasOwnProperty('ingredient')) {
+          if (ingredient.ingredient.toLowerCase().includes(term.toLocaleLowerCase())) {
+            result.push(recipe)
+          }
         }
+      }
+    }
+  }
+  return result
+}
+
+export const search_by_ustensil = (term) => {
+  const result = []
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    if (recipe.hasOwnProperty('ustensils')) {
+      if (recipe.ustensils.includes(term)) {
+        result.push(recipe)
+      }
+    }
+  }
+  return result
+}
+
+export const search_by_appliance = (term) => {
+  const result = []
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    if (recipe.hasOwnProperty('appliance')) {
+      if (recipe.appliance.toLowerCase().includes(term)) {
+        result.push(recipe)
       }
     }
   }
