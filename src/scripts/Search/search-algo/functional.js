7be556e813a2ac77ef8b868575568a25602f1f9c
Callan <cerveauxallanjean@gmail.com>
2021-12-22 19:38:01 +0100

    can search with one more tag

--- src/scripts/Search/search-algo/functional.js
+++ src/scripts/Search/search-algo/functional.js
@@ -1,14 +1,19 @@
+import { compare } from '../../lib/helpers/compare'
 
 export const search_by_name = (arr, term) => arr.filter(({ name }) => name.toLowerCase().includes(term.toLowerCase()))
 
-
 export const search_by_ingredient = (arr, term) => arr.filter(({ ingredients }) => {
-  const result = ingredients.filter(({ ingredient }) => ingredient.toLowerCase().includes(term.toLowerCase()))
-  if (result.length) {
-    return ingredients.includes(...result)
-  }
+  let result = []
+  if (Array.isArray(term)) return ingredients.includes(...compare(term, ingredients))
+  return ingredients.includes(...ingredients.filter(({ ingredient }) => ingredient.toLowerCase().includes(term.toLowerCase())))
 })
 
-export const search_by_ustensil = (arr, term) => arr.filter(({ ustensils }) => ustensils.includes(term))
+export const search_by_ustensil = (arr, term) => arr.filter(({ ustensils }) => {
+  if(Array.isArray(term)) return ustensils.includes(...compare(term, ustensils))
+  return ustensils.includes(term)
+})
 
-export const search_by_appliance = (arr, term) => arr.filter(({ appliance }) => appliance.toLowerCase().includes(term))
+export const search_by_appliance = (arr, term) => arr.filter(({ appliance }) => {
+  if(Array.isArray(term)) return appliance.includes(term)
+  return appliance.toLowerCase().includes(term)
+})
